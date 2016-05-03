8db269b0e161154d0dfab3e9a3b2278a9c5f503e
yuanliang.wu <369002291@qq.com>
2016-05-03 18:19:03 +0800

    Utils commit

---
+++ app/src/main/java/com/wyl/androidstore/utils/FileUtils.java
@@ -0,0 +1,99 @@
+package com.wyl.androidstore.utils;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by Leon Wu on 2016/5/317:06.
+ * Email: yuanliang.wu@weimob.com
+ */
+public class FileUtils {
+
+    //缓存的文件
+    public static final String CACHE_DIR = "cache";
+    //保存图片的路径
+    public static final String ICON_DIR = "icon";
+    //下载文件的路径
+    public static final String DOWNLOAD_DIR = "download";
+    //根目录
+    private static final Object ROOT_DIR = "google";
+
+    public static String getCacheDir() {
+        return getDir(CACHE_DIR);
+    }
+
+    public static String getIconDir() {
+        return getDir(ICON_DIR);
+    }
+
+    public static String getDownloadDir() {
+        return getDir(DOWNLOAD_DIR);
+    }
+
+    private static String getDir(String name) {
+        StringBuilder sb = new StringBuilder();
+        if (isSDCardAvailable()) {
+            sb.append(getExternalStoragePath());
+        } else {
+            sb.append(getCachePath());
+        }
+        sb.append(name);
+        sb.append(File.separator);
+        String path = sb.toString();
+        if (createDirs(path)) {
+            return path;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 创建文件夹
+     * @param path
+     * @return
+     */
+    private static boolean createDirs(String path) {
+        File file=new File(path);
+        if(!file.exists()||!file.isDirectory()){
+            return file.mkdirs();
+        }
+        return true;
+    }
+
+    /**
+     * 获取缓存路径
+     * @return
+     */
+    private static String getCachePath() {
+        File f = UIUtils.getContext().getCacheDir();
+        return f.getAbsolutePath() + "/";
+    }
+
+    /**
+     * 返回SD卡的路径
+     * @return
+     */
+    private static String getExternalStoragePath() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Environment.getExternalStorageDirectory().getAbsolutePath());
+        sb.append(File.separator);
+        sb.append(ROOT_DIR);
+        sb.append(File.separator);
+        return sb.toString();
+    }
+
+    /**
+     * 判断SD卡是否挂载
+     * @return
+     */
+    public static boolean isSDCardAvailable() {
+        if (Environment.MEDIA_MOUNTED.equals(Environment
+                .getExternalStorageState())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/wyl/androidstore/utils/UIUtils.java
@@ -0,0 +1,128 @@
+package com.wyl.androidstore.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import com.wyl.androidstore.BaseActivity;
+import com.wyl.androidstore.BaseApplication;
+
+/**
+ * Created by Leon Wu on 2016/5/317:23.
+ * Email: yuanliang.wu@weimob.com
+ */
+public class UIUtils {
+
+    public static int dip2px(int dip) {
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int) (dip * scale + 0.5f);
+    }
+
+    /**获取上下文*/
+    public static Context getContext() {
+        return BaseApplication.getApplication();
+    }
+
+    /** px--dip */
+
+    public static int px2dip(int px) {
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f);
+    }
+
+    public static Resources getResources() {
+        return getContext().getResources();
+    }
+
+    /** 获取字符数组 */
+    public static String[] getStringArray(int id) {
+        return getResources().getStringArray(id);
+    }
+
+    /** 获取颜色id */
+    public static int getColor(int colorId) {
+        return getResources().getColor(colorId);
+    }
+
+    /** 根据id获取尺寸*/
+    public static int getDimens(int id) {
+        return getResources().getDimensionPixelSize(id);
+    }
+
+    public static View inflate(int id) {
+        return View.inflate(getContext(), id, null);
+    }
+
+    /**
+     * 在主线程中执行代码
+     * @param runnable
+     */
+    public static void runOnUiThread(Runnable runnable) {
+        if (isRunOnMainThread()) {
+            //执行代码
+            runnable.run();
+        } else {
+            post(runnable);
+        }
+    }
+
+
+    public static void post(Runnable runnable) {
+        Handler handler = getHandler();
+        handler.post(runnable);
+    }
+
+    /** 移除一个执行的对象 */
+    public static void removeCallBacks(Runnable r) {
+        getHandler().removeCallbacks(r);
+    }
+
+    /**延迟执行*/
+    public static void postDelay(Runnable runnable, long delay) {
+        Handler handler = getHandler();
+        handler.postDelayed(runnable, delay);
+    }
+
+    private static Handler getHandler() {
+        return BaseApplication.getHandler();
+    }
+
+    public static boolean isRunOnMainThread() {
+        return android.os.Process.myTid() == getMainThreadTid();
+    }
+
+    private static int getMainThreadTid() {
+        return BaseApplication.getMainThreadId();
+    }
+
+    /**
+     * 开启Activity
+     * @param intent
+     */
+    public static void startActivity(Intent intent) {
+        if (BaseActivity.getForegroundActivity() != null) {
+            BaseActivity.getForegroundActivity().startActivity(intent);
+        }else{
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getContext().startActivity(intent);
+        }
+    }
+
+    public static String getString(int id) {
+        return getResources().getString(id);
+    }
+    public static void showToast(String str) {
+        Toast.makeText(UIUtils.getContext(), str, 0).show();
+    }
+    public static void showToast(int id) {
+        Toast.makeText(UIUtils.getContext(), id, 0).show();
+    }
+
+    public static Drawable getDrawable(int id) {
+        return getResources().getDrawable(id);
+    }
+}
