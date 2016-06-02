7e4c6130506bf072b4728334ee5af85c3f11ac3f
yuanliang.wu <369002291@qq.com>
2016-06-02 14:29:42 +0800

    Changes commit

--- app/src/main/java/com/wyl/androidstore/AboutActivity.java
+++ app/src/main/java/com/wyl/androidstore/AboutActivity.java
@@ -1,7 +1,5 @@
 package com.wyl.androidstore;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class AboutActivity extends BaseActivity {
@@ -17,10 +15,5 @@ public class AboutActivity extends BaseActivity {
         super.initActionBar();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-    }
-
-    public static void startActivity(Context context, Class<?> clz) {
-        Intent it = new Intent(context, clz);
-        context.startActivity(it);
     }
 }
